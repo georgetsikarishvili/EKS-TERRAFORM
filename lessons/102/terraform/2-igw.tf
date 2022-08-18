93118ed8bb1aba8461af47f5fea9eebf86235139
gtsikara <g.tsikarishvili@redmed.ge>
2022-08-18 13:05:33 +0400

    [ci skip]

--- lessons/102/terraform/2-igw.tf
+++ lessons/102/terraform/2-igw.tf
@@ -1,7 +1,7 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "EKS-igw" {
+  vpc_id = aws_vpc.EKS.id
 
   tags = {
-    Name = "igw"
+    Name = "EKS-igw"
   }
 }
--- lessons/102/terraform/4-nat.tf
+++ lessons/102/terraform/4-nat.tf
@@ -1,18 +1,18 @@
-resource "aws_eip" "nat" {
+resource "aws_eip" "EKS-nat" {
   vpc = true
 
   tags = {
-    Name = "nat"
+    Name = "EKS-nat"
   }
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public-us-east-1a.id
+  allocation_id = aws_eip.EKS-nat.id
+  subnet_id     = aws_subnet.public-eu-central-1a.id
 
   tags = {
     Name = "nat"
   }
 
-  depends_on = [aws_internet_gateway.igw]
+  depends_on = [aws_internet_gateway.EKS-igw]
 }
